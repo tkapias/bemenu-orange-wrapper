e62f2ba70ed6ace61107c8079b9788b087f5526a
Tomasz Kapias <tomasz@tkapias.net>
2024-11-01 11:33:40 +0700

    fix: no input & fmt

--- bemenu-orange-wrapper.sh
+++ bemenu-orange-wrapper.sh
@@ -11,14 +11,11 @@
 #   Font: Nerd-Fonts Symbols Only
 #   Font: Noto Color Emoji
 #   Font: IBM Plex Mono
-#   bash
 #
 #########################
 
 # using 3 fonts for mono, emokis and nerd-fonts
-pango_fonts="Noto Color Emoji Regular 12, \
-  Symbols Nerd Font Mono Regular 12, \
-  IBM Plex Mono Regular 12"
+pango_fonts="Noto Color Emoji Regular 12, Symbols Nerd Font Mono Regular 12, IBM Plex Mono Regular 12"
 
 # colors
 trans="DF"
@@ -43,43 +40,43 @@ bg_fg_alterning=("$c_bg_alt"         "$c_fg"        )
 bg_fg_scrollbar=("${c_bg_alt}$trans" "${c_fg}$trans")
 
 # global bemenu command location
-if PATH=/usr/bin:/usr/local/bin command -v bemenu 1> /dev/null; then
-  bemenu_cmd=$(PATH=/usr/bin:/usr/local/bin command -v bemenu)
+if PATH=/usr/bin:/usr/local/bin command -v bemenu 1>/dev/null; then
+	bemenu_cmd=$(PATH=/usr/bin:/usr/local/bin command -v bemenu)
 else
-  echo "ERROR: global command bemenu not found!"
-  exit 1
+	echo "ERROR: global command bemenu not found!"
+	exit 1
 fi
 
 # other options
-bemenu_args=(--binding vim \
-    --vim-esc-exits \
-    --ignorecase \
-    --fixed-height \
-    --list 25 \
-    --center \
-    --grab \
-    --line-height 24 \
-    --margin 200 \
-    --scrollbar autohide \
-    --border 3 \
-    --border-radius 0 \
-    --bdr "$fg_border" \
-    --fn  "$pango_fonts" \
-    --tb  "${bg_fg_title[0]}"     --tf  "${bg_fg_title[1]}"     \
-    --fb  "${bg_fg_filter[0]}"    --ff  "${bg_fg_filter[1]}"    \
-    --cb  "${bg_fg_cursor[0]}"    --cf  "${bg_fg_cursor[1]}"    \
-    --nb  "${bg_fg_normal[0]}"    --nf  "${bg_fg_normal[1]}"    \
-    --hb  "${bg_fg_highlight[0]}" --hf  "${bg_fg_highlight[1]}" \
-    --fbb "${bg_fg_feedback[0]}"  --fbf "${bg_fg_feedback[1]}"  \
-    --sb  "${bg_fg_selected[0]}"  --sf  "${bg_fg_selected[1]}"  \
-    --ab  "${bg_fg_alterning[0]}" --af  "${bg_fg_alterning[1]}" \
-    --scb "${bg_fg_scrollbar[0]}" --scf "${bg_fg_scrollbar[1]}")
+bemenu_args=(--binding vim
+	--vim-esc-exits
+	--ignorecase
+	--fixed-height
+	--list 25
+	--center
+	--grab
+	--line-height 24
+	--margin 200
+	--scrollbar autohide
+	--border 3
+	--border-radius 0
+	--bdr "$fg_border"
+	--fn  "$pango_fonts"
+	--tb  "${bg_fg_title[0]}"     --tf  "${bg_fg_title[1]}"
+	--fb  "${bg_fg_filter[0]}"    --ff  "${bg_fg_filter[1]}"
+	--cb  "${bg_fg_cursor[0]}"    --cf  "${bg_fg_cursor[1]}"
+	--nb  "${bg_fg_normal[0]}"    --nf  "${bg_fg_normal[1]}"
+	--hb  "${bg_fg_highlight[0]}" --hf  "${bg_fg_highlight[1]}"
+	--fbb "${bg_fg_feedback[0]}"  --fbf "${bg_fg_feedback[1]}"
+	--sb  "${bg_fg_selected[0]}"  --sf  "${bg_fg_selected[1]}"
+	--ab  "${bg_fg_alterning[0]}" --af  "${bg_fg_alterning[1]}"
+	--scb "${bg_fg_scrollbar[0]}" --scf "${bg_fg_scrollbar[1]}")
 
 # mostly in case of --help or --version
 if [ -p /dev/stdin ]; then
-  cat | "$bemenu_cmd" "${bemenu_args[@]}" "$@"
+	cat | "$bemenu_cmd" "${bemenu_args[@]}" "$@"
 else
-  "$bemenu_cmd" "${bemenu_args[@]}" "$@"
+	printf %s 'No Input' | "$bemenu_cmd" "${bemenu_args[@]}" "$@"
 fi
 
 
